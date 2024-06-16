efc6255ebe270cadc3f6c4ecac0c00c1d383d1be
Chai66 <chaitanyachalumuri06@gmail.com>
2024-06-16 23:17:32 +0530

    daws-76s

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
 output "azs" {  # Output is nothing but will give output to end user wht you goven in output.tf block
     value = data.aws_availability_zones.azs.names
-  
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
